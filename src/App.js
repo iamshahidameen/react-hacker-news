37a016d0a32dcb23dc152746bf3caaed1a016165
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-13 02:11:17 +0500

    Refactored code into components

--- src/App.js
+++ src/App.js
@@ -5,36 +5,9 @@ import Buttons from './Buttons';
 function App() {
   return (
     <>
-      <form className="search-form">
-        <h2>search hacker news</h2>
-        <input type="text" className="form-input" />
-      </form>
-      <div className="btn-container">
-        <button>prev</button>
-        <p>1 of 50</p>
-        <button>next</button>
-      </div>
-      <section className="stories">
-        <article className="story">
-          <h4 className="title">
-            Relicensing React, Jest, Flow, and Immutable.js
-          </h4>
-          <p className="info">
-            2280 points by <span>dwwoelfel | </span> 498 comments
-          </p>
-          <div>
-            <a
-              href="https://code.facebook.com/posts/300798627056246"
-              className="read-link"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              read more
-            </a>
-            <button className="remove-btn">remove</button>
-          </div>
-        </article>
-      </section>
+      <SearchForm />
+      <Buttons />
+      <Stories />
     </>
   );
 }
--- src/Stories.js
+++ src/Stories.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
 
-import { useGlobalContext } from './context'
+import { useGlobalContext } from './context';
 
 const Stories = () => {
-  return <h2>stories component</h2>
-}
+  return (
+    <section className="stories">
+      <article className="story">
+        <h4 className="title">
+          Relicensing React, Jest, Flow, and Immutable.js
+        </h4>
+        <p className="info">
+          2280 points by <span>dwwoelfel | </span> 498 comments
+        </p>
+        <div>
+          <a
+            href="https://code.facebook.com/posts/300798627056246"
+            className="read-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            read more
+          </a>
+          <button className="remove-btn">remove</button>
+        </div>
+      </article>
+    </section>
+  );
+};
 
-export default Stories
+export default Stories;
