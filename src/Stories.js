6fe24e7994ce52ced4ab2914df7f6737347133d7
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-17 01:46:58 +0500

    Display stories

--- src/Stories.js
+++ src/Stories.js
@@ -3,50 +3,35 @@ import React from 'react';
 import { useGlobalContext } from './context';
 
 const Stories = () => {
-  const { isLoading } = useGlobalContext();
+  const { isLoading, hits } = useGlobalContext();
   if (isLoading) {
     return <div className="loading"></div>;
   }
+
   return (
     <section className="stories">
-      <article className="story">
-        <h4 className="title">
-          Relicensing React, Jest, Flow, and Immutable.js
-        </h4>
-        <p className="info">
-          2280 points by <span>dwwoelfel | </span> 498 comments
-        </p>
-        <div>
-          <a
-            href="https://code.facebook.com/posts/300798627056246"
-            className="read-link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            read more
-          </a>
-          <button className="remove-btn">remove</button>
-        </div>
-      </article>
-      <article className="story">
-        <h4 className="title">
-          Relicensing React, Jest, Flow, and Immutable.js
-        </h4>
-        <p className="info">
-          2280 points by <span>dwwoelfel | </span> 498 comments
-        </p>
-        <div>
-          <a
-            href="https://code.facebook.com/posts/300798627056246"
-            className="read-link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            read more
-          </a>
-          <button className="remove-btn">remove</button>
-        </div>
-      </article>
+      {hits.map((story) => {
+        const { title, objectID, author, num_comments, points, url } = story;
+        return (
+          <article className="story" key={objectID}>
+            <h4 className="title">{title}</h4>
+            <p className="info">
+              {points} points by <span>{author}| </span> {num_comments} comments
+            </p>
+            <div>
+              <a
+                href={url}
+                className="read-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                read more
+              </a>
+              <button className="remove-btn">remove</button>
+            </div>
+          </article>
+        );
+      })}
     </section>
   );
 };
