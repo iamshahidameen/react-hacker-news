58ed78e2e34907e4ea19c58ac5fc7ce4445270ca
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-17 01:08:14 +0500

    Basic Functionality added for app in reducer.js and context.js

--- src/Stories.js
+++ src/Stories.js
@@ -3,8 +3,31 @@ import React from 'react';
 import { useGlobalContext } from './context';
 
 const Stories = () => {
+  const { isLoading } = useGlobalContext();
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
   return (
     <section className="stories">
+      <article className="story">
+        <h4 className="title">
+          Relicensing React, Jest, Flow, and Immutable.js
+        </h4>
+        <p className="info">
+          2280 points by <span>dwwoelfel | </span> 498 comments
+        </p>
+        <div>
+          <a
+            href="https://code.facebook.com/posts/300798627056246"
+            className="read-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            read more
+          </a>
+          <button className="remove-btn">remove</button>
+        </div>
+      </article>
       <article className="story">
         <h4 className="title">
           Relicensing React, Jest, Flow, and Immutable.js
